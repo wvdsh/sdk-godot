6c14c0adbad1d5258068ffbd15f85beb9637aa13
Calvin Studebaker <calstudebaker@gmail.com>
2025-06-25 13:18:05 -0700

    feat(WavedashSDK): refactor variable names to use underscore prefix for private variables and add new signals for lobby creation and leaving feat(WavedashSDK): implement sendLobbyChatMsg function to allow sending chat messages in the lobby refactor(WavedashSDK): simplify event handling by creating dedicated functions for lobby events and dispatching JS events

--- wavedash/WavedashSDK.gd
+++ wavedash/WavedashSDK.gd
@@ -5,15 +5,18 @@ var WavedashJS:JavaScriptObject
 
 # Handle events broadcasted from JS to Godot
 # JS -> GD
-var js_callback_receiver:JavaScriptObject
+var _js_callback_receiver:JavaScriptObject
 
 # Handle results when Godot calls async JS functions
 # GD -> JS (async) -> GD
-var on_lobby_joined_js:JavaScriptObject
+var _on_lobby_joined_js:JavaScriptObject
+var _on_lobby_created_js:JavaScriptObject
 
 # Signals that Godot developers can connect to
 signal lobby_joined(payload)
+signal lobby_created(payload)
 signal lobby_message(payload)
+signal lobby_left(payload)
 
 func _ready():
 	if OS.get_name() == "Web":
@@ -21,27 +24,13 @@ func _ready():
 		if not WavedashJS:
 			print("WavedashSDK: WavedashJS not found on window")
 			return
-		on_lobby_joined_js = JavaScriptBridge.create_callback(on_lobby_joined_gd)
-		js_callback_receiver = JavaScriptBridge.create_callback(dispatch_js_event)
-		var engine = JavaScriptBridge.create_object()
+		_on_lobby_joined_js = JavaScriptBridge.create_callback(_on_lobby_joined_gd)
+		_on_lobby_created_js = JavaScriptBridge.create_callback(_on_lobby_created_gd)
+		_js_callback_receiver = JavaScriptBridge.create_callback(_dispatch_js_event)
+		var engine = JavaScriptBridge.create_object("Object")
 		engine["type"] = "Godot"
-		engine["sendMessage"] = js_callback_receiver
+		engine["sendMessage"] = _js_callback_receiver
 		WavedashJS.setEngineInstance(engine)
-
-
-
-# Handle events broadcasted from JS to Godot
-func dispatch_js_event(args):
-	var game_object_name = args[0]  # Unused in Godot. Needed for Unity
-	var method_name = args[1] as String
-	var payload = args[2] as String
-	match method_name:
-		"OnLobbyMessage":
-			var data = JSON.parse_string(payload)
-			lobby_message.emit(data)
-		_:
-			push_warning("[WavedashSDK] Received unknown event from JS: " + method_name)
-
 
 func init(config: Dictionary):
 	if OS.get_name() == "Web" and WavedashJS:
@@ -54,9 +43,38 @@ func get_user():
 
 func join_lobby(lobby_id: String):
 	if OS.get_name() == "Web" and WavedashJS:
-		WavedashJS.joinLobby(lobby_id).then(on_lobby_joined_js)
+		WavedashJS.joinLobby(lobby_id).then(_on_lobby_joined_js)
+		return true
+	return false
 
-func on_lobby_joined_gd(args):
+func sendLobbyChatMsg(lobby_id: String, message: String):
+	if OS.get_name() == "Web" and WavedashJS:
+		# Fire and forget
+		WavedashJS.sendLobbyChatMsg(lobby_id, message)
+		return true
+	return false
+
+# Handle callbacks triggered by Promises resolving
+func _on_lobby_joined_gd(args):
 	var lobby_json = args[0] if args.size() > 0 else null
 	var lobby_data = JSON.parse_string(lobby_json) if lobby_json else {}
 	lobby_joined.emit(lobby_data)
+
+func _on_lobby_created_gd(args):
+	var lobby_json = args[0] if args.size() > 0 else null
+	var lobby_data = JSON.parse_string(lobby_json) if lobby_json else {}
+	lobby_created.emit(lobby_data)
+
+# Handle events broadcasted from JS to Godot
+func _dispatch_js_event(args):
+	print("[GODOT] Received JS Event", args)
+	print("[GODOT] ", args[0])
+	var game_object_name = args[0]  # Unused in Godot. Needed for Unity
+	var method_name = args[1]
+	var payload = args[2]
+	match method_name:
+		"OnLobbyMessage":
+			var data = JSON.parse_string(payload)
+			lobby_message.emit(data)
+		_:
+			push_warning("[WavedashSDK] Received unknown event from JS: " + method_name)
