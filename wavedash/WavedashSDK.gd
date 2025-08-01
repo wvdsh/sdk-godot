6c377231baf208c865fee41d6e24f3f33163cbe0
Calvin Studebaker <calstudebaker@gmail.com>
2025-08-01 03:13:36 -0700

    refactor(WavedashSDK.gd): change user_id type from int to String for better compatibility with user data refactor(WavedashSDK.gd): rename signals for clarity and consistency in naming conventions feat(WavedashSDK.gd): add new function to retrieve leaderboard entries for users feat(WavedashSDK.gd): add function to get leaderboard entry count for better data management refactor(WavedashSDK.gd): improve callback handling for leaderboard results and entries for better readability and maintainability

--- wavedash/WavedashSDK.gd
+++ wavedash/WavedashSDK.gd
@@ -8,12 +8,11 @@ const Constants = preload("WavedashConstants.gd")
 var WavedashJS : JavaScriptObject
 
 # Cache what we can so the next call doesn't have to wait for JS
-var user_id : int = 0
+var user_id : String = ""
 var username : String = ""
 var isReady: bool = false
-# Leaderboard name -> leaderboard data (id, num_entries)
+# Leaderboard id -> leaderboard data (id, num_entries)
 var leaderboard_cache : Dictionary = {}
-var leaderboard_handles : Dictionary = {}
 
 # Handle events broadcasted from JS to Godot
 # JS -> GD
@@ -25,14 +24,16 @@ var _on_lobby_joined_js : JavaScriptObject
 var _on_lobby_created_js : JavaScriptObject
 var _on_get_leaderboard_result_js : JavaScriptObject
 var _on_post_leaderboard_score_result_js : JavaScriptObject
+var _on_get_leaderboard_entries_result_js : JavaScriptObject
 
 # Signals that Godot developers can connect to
 signal lobby_joined(payload)
 signal lobby_created(payload)
 signal lobby_message(payload)
 signal lobby_left(payload)
-signal get_leaderboard_result(payload)
-signal post_leaderboard_score_result(payload)
+signal got_leaderboard(payload)
+signal got_leaderboard_entries(payload)
+signal posted_leaderboard_score(payload)
 
 func _enter_tree():
 	print("WavedashSDK._enter_tree() called, platform: ", OS.get_name())
@@ -45,6 +46,7 @@ func _enter_tree():
 		_on_lobby_joined_js = JavaScriptBridge.create_callback(_on_lobby_joined_gd)
 		_on_lobby_created_js = JavaScriptBridge.create_callback(_on_lobby_created_gd)
 		_on_get_leaderboard_result_js = JavaScriptBridge.create_callback(_on_get_leaderboard_result_gd)
+		_on_get_leaderboard_entries_result_js = JavaScriptBridge.create_callback(_on_get_leaderboard_entries_result_gd)
 		_on_post_leaderboard_score_result_js = JavaScriptBridge.create_callback(_on_post_leaderboard_score_result_gd)
 		_js_callback_receiver = JavaScriptBridge.create_callback(_dispatch_js_event)
 		var engine = JavaScriptBridge.create_object("Object")
@@ -59,8 +61,8 @@ func init(config: Dictionary):
 		print("WavedashSDK: Initializing with config: ", config)
 		WavedashJS.init(JSON.stringify(config))
 
-func get_user_id() -> int:
-	if user_id != 0:
+func get_user_id() -> String:
+	if user_id:
 		return user_id
 		
 	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
@@ -69,7 +71,7 @@ func get_user_id() -> int:
 		username = user_data["username"]
 		return user_id
 	
-	return 0
+	return ""
 	
 func get_username() -> String:
 	if username != "":
@@ -89,6 +91,15 @@ func get_leaderboard(leaderboard_name: String):
 func get_or_create_leaderboard(leaderboard_name: String, sort_method: int, display_type: int):
 	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
 		WavedashJS.getOrCreateLeaderboard(leaderboard_name, sort_method, display_type).then(_on_get_leaderboard_result_js)
+
+func get_leaderboard_entries_for_users(leaderboard_id: String, user_ids: Array[String]):
+	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
+		WavedashJS.getLeaderboardEntriesForUsers(leaderboard_id, user_ids).then(_on_get_leaderboard_entries_result_js)
+
+func get_leaderboard_entry_count(leaderboard_id: String):
+	if leaderboard_cache.has(leaderboard_id):
+		return leaderboard_cache[leaderboard_id]["numEntries"]
+	return -1
 
 func post_leaderboard_score(leaderboard_id: String, keep_best: bool, score: int, metadata: PackedByteArray):
 	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
@@ -113,32 +124,41 @@ func sendLobbyChatMsg(lobby_id: String, message: String):
 
 # Handle callbacks triggered by Promises resolving
 func _on_lobby_joined_gd(args):
-	var lobby_json = args[0] if args.size() > 0 else null
-	var lobby_data = JSON.parse_string(lobby_json) if lobby_json else {}
+	var lobby_json: String = args[0] if args.size() > 0 else null
+	var lobby_data: Dictionary = JSON.parse_string(lobby_json) if lobby_json else {}
 	lobby_joined.emit(lobby_data)
 
 func _on_lobby_created_gd(args):
-	var lobby_id = args[0]
+	var lobby_id: String = args[0]
 	lobby_created.emit(lobby_id)
 
 func _on_get_leaderboard_result_gd(args):
-	var leaderboard_json = args[0] if args.size() > 0 else null
-	var leaderboard_data = JSON.parse_string(leaderboard_json) if leaderboard_json else {}
-	if leaderboard_data.has("id"):
-		leaderboard_cache[leaderboard_data["name"]] = leaderboard_data
-		leaderboard_handles[leaderboard_data["name"]] = leaderboard_data["id"]
-		leaderboard_data["success"] = true
-		get_leaderboard_result.emit(leaderboard_data)
-	else:
-		get_leaderboard_result.emit({"success":false, "name":leaderboard_data["name"]})
+	print("[WavedashSDK] Got leaderboard: ", args)
+	var response_json: String = args[0] if args.size() > 0 else null
+	var response: Dictionary = JSON.parse_string(response_json) if response_json else {}
+	var success: bool = response.get("success", false)
+	if success:
+		leaderboard_cache[response["data"]["id"]] = response["data"]
+	got_leaderboard.emit(response)
+
+func _on_get_leaderboard_entries_result_gd(args):
+	print("[WavedashSDK] Got leaderboard entries: ", args)
+	var response_json: String = args[0] if args.size() > 0 else null
+	var response: Dictionary = JSON.parse_string(response_json) if response_json else {}
+	# Update the leaderboard cache with the total number of entries
+	var success: bool = response.get("success", false)
+	if success:
+		var leaderboard_id: String = response["data"]["leaderboardId"]
+		if leaderboard_cache.has(leaderboard_id):
+			leaderboard_cache[leaderboard_id]["numEntries"] = response["data"]["totalEntries"]
+	
+	got_leaderboard_entries.emit(response)
 
 func _on_post_leaderboard_score_result_gd(args):
-	var leaderboard_json = args[0] if args.size() > 0 else null
-	var leaderboard_data = JSON.parse_string(leaderboard_json) if leaderboard_json else {}
-	if leaderboard_data.has("success"):
-		post_leaderboard_score_result.emit(leaderboard_data)
-	else:
-		post_leaderboard_score_result.emit({"success":false, "name":leaderboard_data["name"]})
+	var response_json: String = args[0] if args.size() > 0 else null
+	var response: Dictionary = JSON.parse_string(response_json) if response_json else {}
+	var success: bool = response.get("success", false)
+	posted_leaderboard_score.emit(response)
 
 # Handle events broadcasted from JS to Godot
 func _dispatch_js_event(args):
