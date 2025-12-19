6452421712df701cd497c5e31f4275eeda110a1b
Calvin Studebaker <calvin@wavedash.gg>
2025-12-19 15:43:15 -0800

    feat(WavedashConstants.gd): add new leaderboard display type for game ticks to enhance leaderboard functionality feat(WavedashSDK.gd): implement functions to signal readiness for events and show overlay in the JS SDK for better event handling and user experience

--- wavedash/WavedashSDK.gd
+++ wavedash/WavedashSDK.gd
@@ -95,6 +95,15 @@ func init(config: Dictionary):
 	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
 		WavedashJS.init(JSON.stringify(config))
 
+# Signal to JS SDK that game is ready to receive events like LOBBY_JOINED, LOBBY_LEFT, etc.
+func ready_for_events() -> void:
+	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
+		WavedashJS.readyForEvents()
+
+func show_overlay() -> void:
+	if OS.get_name() == Constants.PLATFORM_WEB and WavedashJS:
+		WavedashJS.toggleOverlay()
+
 func get_user_id() -> String:
 	if user_id:
 		return user_id
